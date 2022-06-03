eabbaf32585a8c614e8d8afdcc52073d66ae6734
nguyenmanhhieu <hieunm14092001@gmail.com>
2022-06-03 22:47:15 +0700

    dien tich va chu vi hinh tron
    nguyen manh hieu

---
+++ dien tich chu va hinh tron.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  dien tich và chu vi vong tron
+//
+//  Created by Hiếu Hee on 03/06/2022.
+//
+
+#include <stdio.h>
+int main(void)
+{
+    float S;
+    float R;
+    float d;
+    float C;
+        printf("nhap ban kinh\n");
+        scanf("%f",&R);
+    S=(R*R)*3.14;
+        printf("S is %f\n",S);
+    if ( S==(R*R)*3.14 )
+        printf("nhap duong kinh\n");
+        scanf("%f",&d);
+        C=d*3.14;
+        printf("C is %f\n",C);
+    
+   return 0;
+}
